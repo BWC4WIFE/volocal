e50e65401da822198729ef66d938bee09f3cacbb
unknown <mattias@rattelmeier.com>
2026-06-10 17:13:16 +0700

    Add SettingsView and fix mic permission warnings

--- Volocal/Audio/SharedAudioEngine.swift
+++ Volocal/Audio/SharedAudioEngine.swift
@@ -97,9 +97,8 @@ final class SharedAudioEngine: ObservableObject {
     func beginInputCapture() {
         guard let eng = engine, !inputCaptureActive else { return }
 
-        let session = AVAudioSession.sharedInstance()
-        guard session.recordPermission == .granted else {
-            session.requestRecordPermission { [weak self] granted in
+        guard AVAudioApplication.shared.recordPermission == .granted else {
+            AVAudioApplication.requestRecordPermission { [weak self] granted in
                 Task { @MainActor in
                     if granted {
                         self?.beginInputCapture()
